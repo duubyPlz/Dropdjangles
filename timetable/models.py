f0125d5213ebcc5580b004d7b6aafc75624bd7ca
Yan Chi Ku <duubyplz@hotmail.com>
2015-09-17 15:53:13 +1000

    update models.py

--- timetable/models.py
+++ timetable/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.forms import ModelForm
 from swampdragon.models import SelfPublishModel
 from timetable.serializers import TimetableSerializer, ClassSerializer
 
@@ -6,11 +7,34 @@ class Timetable(SelfPublishModel, models.Model):
     serializer_class = TimetableSerializer
     name = models.CharField(max_length=100)
     
+class Course(SelfPublishModel, models.Model):
+    serializer_class = CourseSerializer
+    name = models.CharField(max_length=20)
+    timetable = models.ForeignKey(Timetable)
+
+# https://docs.djangoproject.com/en/1.8/ref/models/fields/
+class ClassType(SelfPublishModel, models.Model):
+    LECTURE = 0
+    TUTORIAL = 1
+    LAB = 2
+    TYPES = (
+        (LECTURE, 'Lecture'),
+        (TUTORIAL, 'Tutorial'),
+        (LAB, 'Lab'),
+    ) 
 
 class Class(SelfPublishModel, models.Model):
     serializer_class = ClassSerializer
     timetable = models.ForeignKey(Timetable)
     name = models.CharField(max_length=10) 
+    classtype = models.IntegerField(choices=TYPES)
     timeFrom = models.IntegerField()
     timeTo = models.IntegerField()
     
+class SearchForm(SelfPublishModel, models.Model):
+    serializer_class = SearchSerializer
+    class Meta:
+        model = Course
+
+
+
