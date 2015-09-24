b8667cf9090a26c2706e5775e0638dc8aceba6b7
Kelvin Li <kelvinlcf@yahoo.com.hk>
2015-09-24 16:57:35 +1000

    table cell fixed size

---
+++ timetable/migrations/0014_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('timetable', '0013_auto_20150923_1618'),
+        ('timetable', '0004_auto_20150924_0541'),
+    ]
+
+    operations = [
+    ]
