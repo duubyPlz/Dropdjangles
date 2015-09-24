d9d01b479c483d53e3cc085229652e1ade378471
Yan Chi Ku <duubyplz@hotmail.com>
2015-09-24 17:25:12 +1000

    change views.py to iterate over all timetables

--- timetable/views.py
+++ timetable/views.py
@@ -7,12 +7,13 @@ from .models import Course, Timetable, Class
 
 def index(request):
     course_list = Course.objects.order_by('name')
-    timetableCourses = Timetable.objects.get(name='15s2').courses.all()
-    class_list = Class.objects.all()
-    template = loader.get_template('../templates/index.html')
-    context = RequestContext(request, {
-        'course_list': course_list,
-        'timetableCourses': timetableCourses,
-        'class_list': class_list,
-    })
+    for obj in Timetable.objects.all(): #for all timetables
+        timetableCourses = obj.courses.all()
+        class_list = Class.objects.all()
+        template = loader.get_template('../templates/index.html')
+        context = RequestContext(request, {
+            'course_list': course_list,
+            'timetableCourses': timetableCourses,
+            'class_list': class_list,
+        })
     return HttpResponse(template.render(context))
