a412676f643241b71ba8d2f1d6e65b25a71bc654
Yan Chi Ku <duubyplz@hotmail.com>
2015-10-15 17:16:50 +1100

    more than one cell

--- src/static/assets/custom_table/timetable.js
+++ src/static/assets/custom_table/timetable.js
@@ -25,18 +25,6 @@ $(document).ready(function() {
         }, function (data) {
             avail_class_list = data.avail_class_list;
             console.log(avail_class_list);
-            
-            function class_on_timetable (col,row) {
-                var i;
-                var r = (row + 8) * 100;
-                for (i = 0; i < avail_class_list.length; i++)
-                    if(avail_class_list[i]['day'] == col-1 &&
-                        avail_class_list[i]['timeFrom'] <= r &&
-                        avail_class_list[i]['timeTo'] > r)
-                        return true;
-                return false;
-            }
-
             // the course and the class type from the sublinks
             // alert(courseId+" , "+classType);
             timetable.children().each(function (row){
@@ -56,9 +44,19 @@ $(document).ready(function() {
     timetable.find('td').click(function () {
         var row = $(this).data('row');
         var col = $(this).data('col');
-        if($(this).hasClass('tableClassSelectingAvail') && !$(this).hasClass('hassClass')){
-            $(this).addClass('hasClass');
-            $(this).html(courseId+"<p>"+classType);
+        var index = which_index(col, row);
+        var hours = class_hours(index);
+        var i;
+        for (i=0; i<hours; i++) {
+            alert(i + " " + hours);
+            var curr_row = row + i;
+            var cell = $('#TimeTable tbody tr').eq(curr_row).find('td').eq(col);
+
+            if(cell.hasClass('tableClassSelectingAvail') && !cell.hasClass('hasClass')){
+                cell.addClass('hasClass');
+                cell.html(courseId+"<p>"+classType);
+                //alert('cell: ' + cell.index() + ' this: ' + $(this).index());
+            }
         }
 
         // remove all the select class tag
@@ -66,4 +64,33 @@ $(document).ready(function() {
         $('td').removeClass('tableClassSelectingNotAvail');
         // alert("You clicked on row " + row + ", col " + col);
     });
+
+    function which_index(col, row) {
+        var timeFrom = (row + 8) * 100;
+        var day = col - 1;
+        var i;
+        for (i = 0; i < avail_class_list.length; i++) {
+            if (avail_class_list[i]['day'] == day
+             && avail_class_list[i]['timeFrom'] == timeFrom) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    function class_on_timetable (col,row) {
+        var i;
+        var r = (row + 8) * 100;
+        for (i = 0; i < avail_class_list.length; i++) {
+            if(avail_class_list[i]['day'] == col-1 &&
+                avail_class_list[i]['timeFrom'] <= r &&
+                avail_class_list[i]['timeTo'] > r)
+                return true;
+        }
+        return false;
+    }
+
+    function class_hours(index) {
+        return Math.floor((avail_class_list[index]['timeTo'] - avail_class_list[index]['timeFrom']) / 100);
+    }
 });
