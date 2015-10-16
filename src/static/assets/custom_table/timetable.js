2c2dfa57657dd35f3b9d958b6340d733311a38d1
Kelvin Li <kelvinlcf@yahoo.com.hk>
2015-10-17 09:44:19 +1100

    update todo

--- src/static/assets/custom_table/timetable.js
+++ src/static/assets/custom_table/timetable.js
@@ -2,23 +2,13 @@
 
 /*
 timetable.js TODO
-
-save added class to backend
-cancel gray
+=========================
 
 add grid to available class (for 2-3 hours class)
 
-merge cell by colspan=“2” or rowspan
-
 move class to a different time
 
-remove class from timetable
-
 class colouring
-
-
-display all class on timetable_class at start
-
 
 auto refresh every two second
 
