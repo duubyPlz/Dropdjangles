14b5bc6cc5787d42531217da85d9d86f204351ed
Kelvin Li <kelvinlcf@yahoo.com.hk>
2015-10-21 18:16:10 +1100

    adf

--- src/static/assets/sidebar/sidebar.js
+++ src/static/assets/sidebar/sidebar.js
@@ -21,7 +21,7 @@ $(function(){
                 var sidebar_links = $('body aside.sidebar-left-collapse div.sidebar-links');
                 var classes_sublinks = "";
                 for (var i = 0; i < data.class_types.length; i++){        
-                    classes_sublinks = classes_sublinks +"<li style='cursor: move;' class='sidebar_classes' id="+required_course_code+"|"+data.class_types[i]+">"+data.class_types[i]+"</li>";
+                    classes_sublinks = classes_sublinks +"<li style='cursor: move;' class='sidebar_classes draggable' id="+required_course_code+"|"+data.class_types[i]+">"+data.class_types[i]+"</li>";
                 }
                 if(data.valid) {
                     sidebar_links.append(" \
