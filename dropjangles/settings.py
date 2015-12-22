e0360a9146f9ffabf13d14b43ba8953526234feb
Kelvin Li <kelvinlcf@yahoo.com.hk>
2015-12-22 21:05:23 +1100

    heroku

--- dropjangles/settings.py
+++ dropjangles/settings.py
@@ -90,16 +90,16 @@ WSGI_APPLICATION = 'dropjangles.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'timetable',
-#         'USER': 'admin',
-#         'PASSWORD': 'admin',
-#         'HOST': 'localhost',
-#         'PORT': '',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'timetable',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
 
 import dj_database_url
 DATABASES = {}
