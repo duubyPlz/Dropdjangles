bc1ff5e5b719a24e8d5dbc9d7d1c6aca95a91e5d
Kelvin Li <kelvinlcf@yahoo.com.hk>
2016-02-29 17:00:57 +1100

    changed db

--- src/dropjangles/settings.py
+++ src/dropjangles/settings.py
@@ -104,9 +104,9 @@ DATABASES = {
 }
 
 # This is for using heroku database
-import dj_database_url
-DATABASES = {}
-DATABASES = {'default': dj_database_url.config()}
+# import dj_database_url
+# DATABASES = {}
+# DATABASES = {'default': dj_database_url.config()}
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
